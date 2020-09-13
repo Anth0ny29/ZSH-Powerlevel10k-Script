758a30841377175d1a70772b7d43341fce0ff597
Anth0ny29 <43807800+Anth0ny29@users.noreply.github.com>
2020-09-13 09:47:08 +0200

    Script original

---
+++ ZSH-Powerlevel10k.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+NOCOLOR='\033[0m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+CheckSudo()
+{
+
+if [ $EUID -eq 0 ]; then
+	echo -e "${RED}Le script ne peut pas lancer avec sudo"
+
+    exit $?
+
+fi
+}
+
+Install_Packages()
+{
+
+sudo apt update && sudo apt install -y zsh curl git
+wait
+
+if [ $? -eq 0 ]
+then
+
+        echo -e "${YELLOW}Les paquets nécessaires sont installés, changement de l'interpréteur de commandes...${NOCOLOR}"
+        chsh -s /bin/zsh
+		if [ $? -eq 1 ]; then
+			echo -e "${RED}Erreur, veuillez réessayer${NOCOLOR}"
+			chsh -s /bin/zsh
+		fi
+fi
+}
+
+if CheckSudo -eq 0; then
+
+	echo -e "${YELLOW}Vérification de l'installation des paquets nécessaires...${NOCOLOR}" && Install_Packages
+	
+	echo -e "${YELLOW}Installation de Oh-My-ZSH...${NOCOLOR}"
+		
+		if [ ! -d "$HOME/.oh-my-zsh" ]; then
+			echo -e "${YELLOW}Oh-My-ZSH non installé, installation...${NOCOLOR}"
+			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"> /dev/null&
+			wait
+			echo "Oh-My-ZSH installé"
+		else
+			echo -e "${YELLOW}Oh-My-ZSH est déjà installé, réinstallation...${NOCOLOR}"
+			rm -Rf $HOME/.oh-my-zsh
+			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"> /dev/null&
+			wait
+			echo "Oh-My-ZSH installé"
+
+		fi
+	
+		if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ];then
+		
+			echo -e "${YELLOW}Installation du plugin zsh-syntax-highlighting${NOCOLOR}"
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting
+			mv zsh-syntax-highlighting/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-syntax-highlighting installé${NOCOLOR}"
+	
+		else
+			echo -e "${YELLOW}ré-installation du plugin zsh-syntax-highlighting${NOCOLOR}"
+			rm -Rf $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting
+			mv zsh-syntax-highlighting/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-syntax-highlighting installé${NOCOLOR}"
+		fi
+
+		if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-completions" ]
+		then
+			echo -e "${YELLOW}Installation du plugin zsh-completions${NOCOLOR}"
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-completions
+			mv zsh-completions/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-completions installé${NOCOLOR}"
+		
+		else
+			echo -e "${YELLOW}ré-installation du plugin zsh-completions${NOCOLOR}"
+			rm -Rf $HOME/.oh-my-zsh/plugins/zsh-completions
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-completions
+			mv zsh-completions/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-completions installé${NOCOLOR}"
+		fi
+
+		if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]
+		then
+			echo -e "${YELLOW}Installation du plugin zsh-autosuggestions${NOCOLOR}"
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-autosuggestions
+			mv zsh-autosuggestions/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-autosuggestions installé${NOCOLOR}"
+		
+		else
+			echo -e "${YELLOW}ré-installation du plugin zsh-autosuggestions${NOCOLOR}"
+			rm -Rf $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
+			cd /tmp/ && git clone --quiet https://github.com/zsh-users/zsh-autosuggestions
+			mv zsh-autosuggestions/ $HOME/.oh-my-zsh/plugins
+			echo -e "${GREEN}Plugin zsh-autosuggestions installé${NOCOLOR}"
+		fi
+	
+	
+	sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-completions zsh-autosuggestions)/' $HOME/.zshrc
+	
+	if [ ! -d "$HOME/.oh-my-zsh/plugins/powerlevel10k" ]
+		then
+			echo -e "${YELLOW}Installation du thème Powerlevel10k...${NOCOLOR}"
+			cd /tmp/ && git clone --quiet https://github.com/romkatv/powerlevel10k
+			mv powerlevel10k/ $HOME/.oh-my-zsh/themes/
+			sed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' $HOME/.zshrc
+			echo -e "${GREEN}Plugin zsh-autosuggestions installé${NOCOLOR}"
+			echo -e "${GREEN}Thème Powerlevel10k installé${NOCOLOR}"
+		else
+			echo -e "${YELLOW}Installation du thème Powerlevel10k...${NOCOLOR}"
+			rm -Rf $HOME/.oh-my-zsh/themes/powerlevel10k
+			cd /tmp/ && git clone --quiet https://github.com/romkatv/powerlevel10k
+			mv powerlevel10k/ $HOME/.oh-my-zsh/themes/
+			sed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' $HOME/.zshrc
+			echo -e "${GREEN}Thème Powerlevel10k installé${NOCOLOR}"
+		fi
+		
+	
+	
+	
+		
+	
+
+	echo -e "${GREEN}Installation terminée ! Pour appliquer les changements, veuillez vous déconnecter de la session actuelle et vous reconnecter${NOCOLOR}"
+	
+else 
+
+	echo -e "${RED}Le script ne peut pas lancer avec sudo"
+	exit
+	
+fi 
